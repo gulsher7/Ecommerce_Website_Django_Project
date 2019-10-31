c081efc4762b5b9b062e4cfa1ed1bdb4b134c2e0
gulsher <gulsherkhan0005@gmail.com>
2019-10-31 20:04:41 +0530

    commit

--- shop/models.py
+++ shop/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 
 class Contact(models.Model):
 	user_id = models.AutoField(primary_key=True)
-	user_name=models.CharField(max_length=50, blank=True, null=True)
-	user_email=models.EmailField(max_length=50, blank=True, null=True)
-	user_phone=models.CharField(max_length=20, blank=True, null=True)
-	user_desc=models.CharField(max_length=200, blank=True, null=True)
+	user_name=models.CharField(max_length=50, blank=False)
+	user_email=models.EmailField(max_length=50, blank=False)
+	user_phone=models.CharField(max_length=20, blank=False)
+	user_desc=models.CharField(max_length=200, blank=False)
 
 	def __str__(self):
 		return self.user_name
@@ -15,9 +15,9 @@ class Contact(models.Model):
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=50, blank=True, null=True)
-    product_price = models.CharField(max_length=10, blank=True, null=True)
-    desc = models.TextField(blank=True, null=True)
+    product_name = models.CharField(max_length=256, blank=False)
+    product_price = models.IntegerField(blank=False)
+    desc = models.TextField(blank=False)
     product_image= models.ImageField(upload_to='post_image', blank=True, null=True)
 
     def __str__(self):
